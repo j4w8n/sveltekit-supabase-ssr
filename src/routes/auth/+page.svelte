24e9c67d146a92a40c842a16c841ed8e45720015
Jason Creviston <jason@malynium.com>
2023-11-12 15:38:18 -0500

    chore: remove client-side login methods

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,17 +1,5 @@
 <script lang="ts">
-  import type { Provider } from '@supabase/supabase-js'
-
-  export let data, form
-
-  let { supabase } = data
-  $: ({ supabase } = data)
-
-  const signInWithOAuth = async (provider: Provider) => {
-    const { error } = await supabase.auth.signInWithOAuth({ 
-      provider
-    })
-    if (error) console.error(error)
-  }
+  export let form
 </script>
 
 {#if form?.message}
@@ -27,4 +15,3 @@
 <form method="POST" action="?/signin">
   <button style="margin-top: 12px;" name="provider" value="github">Login with GitHub ServerSide</button>
 </form>
-<button style="margin-top: 12px;" on:click={() => signInWithOAuth('github')}>Login with GitHub ClientSide</button>
