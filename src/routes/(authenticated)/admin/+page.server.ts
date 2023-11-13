4b907075d90ebb99b9751de670a03294c7fc7daa
Jason Creviston <jason@malynium.com>
2023-11-13 07:05:33 -0500

    fix: session not updating on page

--- src/routes/(authenticated)/admin/+page.server.ts
+++ src/routes/(authenticated)/admin/+page.server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit'
 
-export const load = async ({ locals: { getSession } }) => {
+export const load = async ({ locals: { getSession }, depends }) => {
+  depends('supabase:auth')
+  
   const session = await getSession()
 
   if (!session) throw redirect(307, '/auth')
