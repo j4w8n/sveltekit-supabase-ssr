9a207d0e5c2942acf43c00ff432d3af60a014407
Jason Creviston <jason@malynium.com>
2024-03-22 15:47:50 -0400

    refactor: pesky semicolons

--- src/routes/(authenticated)/admin/+page.server.ts
+++ src/routes/(authenticated)/admin/+page.server.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 export const load = async ({ locals: { getSession } }) => {
   const session = await getSession()
 
-  if (!session) redirect(307, '/auth');
+  if (!session) redirect(307, '/auth')
 
   /* Ensures the session, sourced from a cookie, is not fake. See README for details. */
   jwt.verify(session.access_token, JWT_SECRET, (err) => { if (err) redirect(307, '/auth') })
