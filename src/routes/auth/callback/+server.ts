1adbb230cf311d3603c9414bd66a820bb9f54210
Jason Creviston <jason@malynium.com>
2024-05-24 17:42:16 -0400

    refactor: destructure event sooner

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,10 +1,6 @@
 import { redirect } from '@sveltejs/kit'
 
-export const GET = async (event) => {
-	const {
-		url,
-		locals: { supabase }
-	} = event;
+export const GET = async ({ url, locals: { supabase } }) => {
 	const code = url.searchParams.get('code') as string
 	const next = url.searchParams.get('next') ?? '/'
 
--- src/routes/auth/confirm/+server.ts
+++ src/routes/auth/confirm/+server.ts
@@ -1,11 +1,7 @@
 import type { EmailOtpType } from '@supabase/supabase-js'
 import { redirect } from '@sveltejs/kit'
 
-export const GET = async (event) => {
-	const {
-		url,
-		locals: { supabase }
-	} = event
+export const GET = async ({ url, locals: { supabase } }) => {
 	const token_hash = url.searchParams.get('token_hash') as string
 	const type = url.searchParams.get('type') as EmailOtpType
 	const next = url.searchParams.get('next') ?? '/'
