ce446a5723bfb1801984009ffa50bab4680f7eb9
Jason Creviston <jason@malynium.com>
2024-05-24 22:00:36 -0400

    refactor: tidy up

--- src/routes/(authenticated)/self/+page.svelte
+++ src/routes/(authenticated)/self/+page.svelte
@@ -8,10 +8,10 @@
 {#if session}
   <h2>Welcome to /self!</h2>
   <h4>Your id is {session.user.id}</h4>
-  <h4>Your nickname is {session.user.user_metadata.nickname ?? 'not set'}</h4>
+  <h4>Your nickname is {session.user.user_metadata.nickname ?? "not set"}</h4>
   <form method="POST" action="?/update">
     Change your nickname:
-    <input name="nickname" type="text" value={ form?.data.nickname ?? ''}>
+    <input name="nickname" type="text" value={ form?.data.nickname ?? ""}>
     <button style="margin-top: 12px;">Update</button>
     <button formaction="?/delete" style="margin-top: 12px;">Delete</button>
   </form>
