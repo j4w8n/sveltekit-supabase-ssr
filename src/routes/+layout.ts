3a574e3d0a52f747636551e6599c792a091b469d
Jason Creviston <jason@malynium.com>
2024-05-30 12:53:50 -0400

    fix: create separate clients in layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,30 +1,29 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
-import { createBrowserClient, isBrowser, parse } from '@supabase/ssr'
+import { createBrowserClient, createServerClient, isBrowser, parse } from '@supabase/ssr'
 
 export const load = async ({ fetch, data, depends }) => {
   depends('supabase:auth')
 
-  const supabase = createBrowserClient(
-    PUBLIC_SUPABASE_URL,
-    PUBLIC_SUPABASE_ANON_KEY,
-    {
-      global: { fetch },
-      cookies: {
-        get(key) {
-          if (!isBrowser()) {
+  const supabase = isBrowser()
+    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: { fetch },
+        cookies: {
+          get(key) {
+            const cookie = parse(document.cookie)
+            return cookie[key]
+          }
+        }
+      })
+    : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+        global: { fetch },
+        cookies: {
+          get() { 
             return JSON.stringify(data.session)
           }
-  
-          const cookie = parse(document.cookie)
-          return cookie[key]
         }
-      }
-    }
-  )
+      })
 
-  const {
-    data: { session }
-  } = await supabase.auth.getSession()
+  const session = isBrowser() ? (await supabase.auth.getSession()).data.session : data.session
 
   return { supabase, session }
 }
